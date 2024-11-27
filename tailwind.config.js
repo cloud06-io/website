d5bdb87bb7362377d76bd674da92fac91b81f824
fhenri <fhenri@fhsolution.com>
2024-11-27 06:43:18 +0100

    add business card QR code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: [
+    "./*.{html,js}",
+    './card/*.{html,js}',
+  ],
   theme: {
     backgroundPosition: {
       'center-center': 'center center',
